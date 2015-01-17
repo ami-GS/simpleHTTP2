a141df208f472dffc5ce456f1f898a2324c23548
Daiki Aminaka <1991.daiki@gmail.com>
2015-01-17 21:07:34 +0900

    adjust the way to construct frames based on previous commit

--- session.go
+++ session.go
@@ -17,14 +17,26 @@ func (self *Session) Parse(buf []byte) {
 	info.Parse(buf[:9])
 
 	if info.Type == TYPE_DATA {
-		data := Data{}
-		data.Parse(buf[9:], info.Flag, info.Length)
+		data := Data{Header: &info}
+		data.Parse(buf[9:])
 		fmt.Printf("data: %s", data.Data)
 	} else if info.Type == TYPE_HEADERS {
-		headers := Headers{}
-		headers.Parse(buf[9:], info.Flag, &self.Table)
+		headers := Headers{Header: &info}
+		var idx, padLen byte = 0, 0
+		if info.Flag&FLAG_PADDED == FLAG_PADDED {
+			padLen = buf[9]
+			idx += 1 + padLen
+		}
+		if info.Flag&FLAG_PRIORITY == FLAG_PRIORITY {
+			idx += 5
+		}
+		header := hpack.Decode(buf[idx:byte(len(buf[9:]))-padLen], &self.Table)
+
+		headers.Parse(buf[9:])
+		headers.Headers = header
 		if info.Flag == FLAG_END_HEADERS {
-			self.Send(NewData("Hello! DATA frame", 1, FLAG_PADDED, 5))
+			frame := Frame(NewData("Hello! DATA frame", 1, FLAG_PADDED, 5))
+			self.Send(frame.GetWire())
 		}
 		fmt.Println("headers")
 	} else if info.Type == TYPE_PRIORITY {
@@ -32,10 +44,11 @@ func (self *Session) Parse(buf []byte) {
 	} else if info.Type == TYPE_RST_STREAM {
 		fmt.Println("rst stream")
 	} else if info.Type == TYPE_SETTINGS {
-		settings := Settings{}
-		settings.Parse(buf[9:], info.Flag)
+		settings := Settings{Header: &info}
+		settings.Parse(buf[9:])
 		if info.Flag == FLAG_NO {
-			self.Send(NewSettings(SETTINGS_NO, 0, FLAG_ACK))
+			frame := Frame(NewSettings(SETTINGS_NO, 0, FLAG_ACK))
+			self.Send(frame.GetWire())
 		} else if info.Flag == FLAG_ACK {
 			fmt.Println("recv ACK setting!")
 		}
@@ -43,7 +56,7 @@ func (self *Session) Parse(buf []byte) {
 	} else if info.Type == TYPE_PING {
 		fmt.Println("ping")
 	} else if info.Type == TYPE_GOAWAY {
-		goaway := GoAway{}
+		goaway := GoAway{Header: &info}
 		goaway.Parse(buf[9:])
 		fmt.Printf("goaway: %s", goaway.Debug)
 	} else if info.Type == TYPE_WINDOW_UPDATE {
