1b19d3db102660cf94eb09da72daf3d4cf83f669
Daiki Aminaka <1991.daiki@gmail.com>
2015-02-24 20:18:09 +0900

    use switch instead of else-if chain

--- session.go
+++ session.go
@@ -17,10 +17,11 @@ func (self *Session) Parse(buf []byte) {
 	info.Parse(buf[:9])
 
 	var frame Frame
-	if info.Type == DATA_FRAME {
+	switch info.Type {
+	case DATA_FRAME:
 		frame = &Data{Header: &info}
 		frame.Parse(buf[9:])
-	} else if info.Type == HEADERS_FRAME {
+	case HEADERS_FRAME:
 		var idx, padLen byte = 0, 0
 		if info.Flag&PADDED == PADDED {
 			padLen = buf[9]
@@ -36,26 +37,26 @@ func (self *Session) Parse(buf []byte) {
 		if info.Flag == END_HEADERS {
 			self.Send(NewData("Hello! DATA frame", 1, PADDED, 5))
 		}
-	} else if info.Type == PRIORITY_FRAME {
+	case PRIORITY_FRAME:
 		frame = &Priority{Header: &info}
 		frame.Parse(buf[9:])
-	} else if info.Type == RST_STREAM_FRAME {
-	} else if info.Type == SETTINGS_FRAME {
+	case RST_STREAM_FRAME:
+	case SETTINGS_FRAME:
 		frame = &Settings{Header: &info}
 		frame.Parse(buf[9:])
 		if info.Flag == NO {
 			self.Send(NewSettings(NO_SETTING, 0, ACK))
 		} else if info.Flag == ACK {
 		}
-	} else if info.Type == PING_FRAME {
+	case PING_FRAME:
 		frame = &Ping{Header: &info}
 		frame.Parse(buf[9:])
-	} else if info.Type == GOAWAY_FRAME {
+	case GOAWAY_FRAME:
 		frame = &GoAway{Header: &info}
 		frame.Parse(buf[9:])
-	} else if info.Type == WINDOW_UPDATE_FRAME {
-	} else if info.Type == CONTINUATION_FRAME {
-	} else {
+	case WINDOW_UPDATE_FRAME:
+	case CONTINUATION_FRAME:
+	default:
 		panic("undefined frame type")
 	}
 	fmt.Printf("Receive: \n%s\n", frame.String())
