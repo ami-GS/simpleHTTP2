e6cd61518b0bd62bfe20583a1198f9d037f6c369
Daiki Aminaka <1991.daiki@gmail.com>
2015-03-04 23:08:11 +0900

    added connection to stream

--- stream.go
+++ stream.go
@@ -1,14 +1,15 @@
 package http2
 
 type Stream struct {
+	connection *Connection
 	ID         uint32
 	WindowSize uint16
 	State      STATE
 }
 
 // 65535 should be defined in connection
-func NewStream(streamID uint32) *Stream {
-	return &Stream{streamID, 65535, IDLE}
+func NewStream(connection *Connection, streamID uint32) *Stream {
+	return &Stream{connection, streamID, 65535, IDLE}
 }
 
 func (self *Stream) ChangeState(state STATE) {
@@ -17,4 +18,8 @@ func (self *Stream) ChangeState(state STATE) {
 
 func (self *Stream) DecreaseWindow(size uint16) {
 	self.WindowSize -= size
+}
+
+func (self *Stream) Send(frame Frame) {
+	self.connection.Send(frame)
 }
